51def2b2a817063cd43558e6cb5c53dd25029ef0
krossovochkin <1792204+krossovochkin@users.noreply.github.com>
2024-11-29 19:15:38 +0000

    [create-pull-request] automated change

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,6 +8,7 @@ pluginManagement {
 plugins {
     // See https://jmfayard.github.io/refreshVersions
     id("de.fayard.refreshVersions") version "0.60.4"
+////                            # available:"0.60.5"
 }
 
 rootProject.name = "krossovochkin-web-apps"
